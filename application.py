eb40a768363251ff4b1b1c377bd9e9472369211c
Kodjo Edem KOUNTA <kkounta@OTR.LAN>
2024-07-03 18:46:41 +0000

    First implementation of chebyshev_distance

--- application.py
+++ application.py
@@ -7,3 +7,15 @@ if __name__ == "__main__":
     point2 = (4, 5, 6)
     distance = chebyshev_distance(point1, point2)
     print("Chebyshev Distance:", distance)
+
+
+# First method
+def chebyshev_distance(p1, p2):
+    """Calculate Chebyshev distance using map and lambda."""
+    return max(map(lambda x_y: abs(x_y[0] - x_y[1]), zip(p1, p2)))
+
+if __name__ == "__main__":
+    point1 = (1, 2, 3)
+    point2 = (4, 5, 6)
+    distance = chebyshev_distance(point1, point2)
+    print("Chebyshev Distance:", distance)
